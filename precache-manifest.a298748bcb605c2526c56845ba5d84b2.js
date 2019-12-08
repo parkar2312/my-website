9170b1d78125ce8858021056b42160cf1709713c
parimal <chandrakarparimal2@gmail.com>
2019-12-08 22:39:47 +0530

    Updates

---
+++ precache-manifest.a298748bcb605c2526c56845ba5d84b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a136a5fb811e8bb0e8f91ec1148f4b",
+    "url": "/parkar2312/my-website.git/index.html"
+  },
+  {
+    "revision": "8e4137609720a92eea36",
+    "url": "/parkar2312/my-website.git/static/css/main.f1ca7409.chunk.css"
+  },
+  {
+    "revision": "ff6c8ed18d84c0432d35",
+    "url": "/parkar2312/my-website.git/static/js/2.01257c59.chunk.js"
+  },
+  {
+    "revision": "8e4137609720a92eea36",
+    "url": "/parkar2312/my-website.git/static/js/main.a7581d1c.chunk.js"
+  },
+  {
+    "revision": "2ac9f739d4baf3b37466",
+    "url": "/parkar2312/my-website.git/static/js/runtime-main.9d2e792f.js"
+  },
+  {
+    "revision": "87dc597295e33e2d8739db51f2513737",
+    "url": "/parkar2312/my-website.git/static/media/WHAT-IS-ECOMMERCE-WEB-DEVELOPMENT-1.87dc5972.jpg"
+  },
+  {
+    "revision": "057d972cbaf24e2ab2df113958d1d617",
+    "url": "/parkar2312/my-website.git/static/media/backjs.057d972c.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/parkar2312/my-website.git/static/media/logo.25bf045c.svg"
+  }
+]);
